b7e739a841aa4d08d0b67c559c8023c4fa56d1b7
Chetan Sharma <chetan2309@gmail.com>
2022-12-26 09:44:39 +0000

    Committed code for guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello world");
+    println!("Guess the number!");
+
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read a line");
+
+        // let guess: u32 = guess.trim().parse().expect("Please provide a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // println!("The secret guess is: {secret_num}");
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }    
+    }
+    
 }
